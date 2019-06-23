eba54c359163808bafdcc7b0c48dff64029e5cfa
E-Dreamer <939902197@qq.com>
2019-06-23 11:41:43 +0800

    首页布局 注册50%

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+
+
 // 设置 默认采用的模板引擎名称
 app.set('view engine', 'ejs')
     // 设置模板页面的存放路径
@@ -8,6 +11,7 @@ app.set('views', './views')
 
 app.use('/node_modules', express.static('node_modules'))
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index', {})
@@ -20,6 +24,12 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('./user/login', {})
 })
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.send({ status: 200, msg: 'ok' });
+})
+
 app.listen(80, () => {
     console.log("服务器运行成功……")
 })
